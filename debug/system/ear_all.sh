ecb57504364da92d8bfb8706579150c51c5316f5
karlzheng <zhengkarl@gmail.com>
2013-04-10 16:09:01 +0800

    add tinyalsa arm bin files and alsa path setting files

---
+++ debug/system/ear_all.sh
@@ -0,0 +1,256 @@
+tinymix 0   12
+tinymix 1   12
+tinymix 2   12
+tinymix 3   12
+tinymix 4   12
+tinymix 5   12
+tinymix 6   12
+tinymix 7   12
+tinymix 8   12
+tinymix 9   12
+tinymix 10  12
+tinymix 11  12
+tinymix 12  12
+tinymix 13  12
+tinymix 14  12
+tinymix 22  27
+tinymix 25  27
+tinymix 28  13
+tinymix 31  11
+tinymix 34  1
+tinymix 35  0
+tinymix 36  0
+tinymix 37  0
+tinymix 38  0
+tinymix 39  0
+tinymix 40  0
+tinymix 41  0
+tinymix 42  0
+tinymix 43  0
+tinymix 44  7
+tinymix 45  7
+tinymix 46  7
+tinymix 47  7
+tinymix 48  7
+tinymix 49  7
+tinymix 50  7
+tinymix 51  7
+tinymix 52  7
+tinymix 53  7
+tinymix 54  7
+tinymix 55  7
+tinymix 56  7
+tinymix 57  7
+tinymix 58  7
+tinymix 59  7
+tinymix 60  61 61
+tinymix 64  1
+tinymix 65  1
+tinymix 66  1
+tinymix 67  1
+tinymix 68  1
+tinymix 69  1
+tinymix 70  1
+tinymix 71  3 3
+tinymix 72  59 59
+tinymix 75  0 0
+tinymix 78  39 39
+tinymix 83  1
+tinymix 86  1
+tinymix 87  96 96
+tinymix 88  96 96
+tinymix 89  96 96
+tinymix 100 96 96
+tinymix 101 96 96
+tinymix 102 96 96
+tinymix 103 0
+tinymix 104 0
+tinymix 117 0
+tinymix 118 0
+tinymix 119 12
+tinymix 120 12
+tinymix 129 96 96
+tinymix 131 96 96
+tinymix 133 1
+tinymix 134 1
+tinymix 135 1
+tinymix 136 1
+tinymix 137 0
+tinymix 139 0
+tinymix 141 0
+tinymix 143 2
+
+tinymix 15	 1
+tinymix 16	 0
+tinymix 17	 0
+tinymix 18	 0
+tinymix 23	 1
+tinymix 24	 0
+tinymix 26	 1
+tinymix 27	 0
+tinymix 29	 0
+tinymix 30	 0
+tinymix 32	 0
+tinymix 33	 0
+tinymix 61	 1 1
+tinymix 62	 1 1
+tinymix 63	 0
+tinymix 73	 0 0
+tinymix 74	 0 0
+tinymix 79	 1 1
+tinymix 80	 1 1
+tinymix 81	 0
+tinymix 82	 0
+tinymix 84	 0
+tinymix 85	 0
+tinymix 105	 1
+tinymix 106	 0
+tinymix 107	 0
+tinymix 108	 0
+tinymix 109	 0
+tinymix 110	 0
+tinymix 111	 0
+tinymix 112	 0
+tinymix 113	 0
+tinymix 114	 0
+tinymix 115	 0
+tinymix 116	 0
+tinymix 122	 0
+tinymix 124	 0 0
+tinymix 126	 0 0
+tinymix 128	 1 1
+tinymix 130	 1 1
+tinymix 132	 1 1
+tinymix 138	 0
+tinymix 140	 0
+tinymix 142	 0
+tinymix 144	 0
+tinymix 145	 0
+tinymix 146	 0
+tinymix 147	 0
+tinymix 148	 0
+tinymix 149	 0
+tinymix 150	 0
+tinymix 151	 0
+tinymix 152	 0
+tinymix 153	 0
+tinymix 154	 0
+tinymix 155	 0
+tinymix 156	 0
+tinymix 157	 0
+tinymix 158	 0
+tinymix 166	 0
+tinymix 167	 0
+tinymix 168	 0
+tinymix 169	 0
+tinymix 170	 1
+tinymix 171	 0
+tinymix 172	 0
+tinymix 173	 0
+tinymix 174	 0
+tinymix 175	 1
+tinymix 181	 0
+tinymix 182	 0
+tinymix 183	 1
+tinymix 184	 0
+tinymix 185	 1
+tinymix 186	 0
+tinymix 187	 0
+tinymix 188	 1
+tinymix 189	 0
+tinymix 190	 1
+tinymix 193	 1
+tinymix 194	 0
+tinymix 195	 0
+tinymix 196	 0
+tinymix 197	 0
+tinymix 198	 0
+tinymix 199	 1
+tinymix 200	 0
+tinymix 201	 0
+tinymix 202	 0
+tinymix 203	 0
+tinymix 204	 0
+tinymix 205	 0
+tinymix 206	 0
+tinymix 207	 0
+tinymix 208	 0
+tinymix 209	 0
+tinymix 210	 0
+tinymix 211	 0
+tinymix 212	 0
+tinymix 213	 0
+tinymix 214	 0
+tinymix 215	 0
+tinymix 216	 0
+tinymix 217	 0
+tinymix 218	 0
+tinymix 219	 0
+tinymix 220	 0
+tinymix 221	 1
+tinymix 222	 1
+tinymix 223	 0
+tinymix 224	 1
+tinymix 225	 1
+tinymix 226	 0
+tinymix 227	 0
+tinymix 228	 0
+tinymix 229	 0
+tinymix 230	 0
+tinymix 231	 0
+tinymix 232	 0
+tinymix 233	 1
+tinymix 234	 0
+tinymix 235	 0
+tinymix 236	 0
+tinymix 237	 0
+tinymix 238	 0
+tinymix 239	 0
+tinymix 240	 0
+tinymix 241	 1
+tinymix 242	 0
+tinymix 243	 1
+tinymix 244	 0
+tinymix 245	 1
+tinymix 246	 0
+tinymix 247	 0
+tinymix 248	 1
+tinymix 249	 1
+tinymix 250	 1
+tinymix 251	 1
+tinymix 252	 1
+tinymix 253	 1
+
+tinymix 19	"Headphone  Nomal"
+tinymix 20	"None"
+tinymix 21	"On"
+tinymix 76	"SPKVDD/2"
+tinymix 77	"Class D"
+tinymix 90	"Low Power"
+tinymix 91	"High Performance"
+tinymix 92	"Left"
+tinymix 93	"Right"
+tinymix 94	"Left"
+tinymix 95	"Right"
+tinymix 96	"Left"
+tinymix 97	"Right"
+tinymix 98	"Left"
+tinymix 99	"Right"
+tinymix 121	"2.7kHz"
+tinymix 123	"HiFi"
+tinymix 125	"HiFi"
+tinymix 127	"Voice 1"
+tinymix 159	"playback spk normal"
+tinymix 160	"ADC"
+tinymix 161	"ADC"
+tinymix 162	"AIF1ADCDAT"
+tinymix 163	"AIF2"
+tinymix 164	"AIF2"
+tinymix 165	"None"
+tinymix 176	"DAC"
+tinymix 177	"DAC"
+tinymix 178	"AIF2ADCDAT"
+tinymix 179	"AIF2DACDAT"
+tinymix 180	"AIF1DACDAT"
+tinymix 191	"ADC/DMIC1"
+tinymix 192	"ADC/DMIC1"
